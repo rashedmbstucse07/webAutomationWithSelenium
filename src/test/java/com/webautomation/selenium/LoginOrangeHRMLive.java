414358faf23ec72e361b9858883db83b47dad841
rashedul.islam <rashedmbstucse07@gmail.com>
2022-08-07 00:50:53 +0600

    Added LoginOrangeHRMLive class

--- src/test/java/com/webautomation/selenium/LoginOrangeHRMLive.java
+++ src/test/java/com/webautomation/selenium/LoginOrangeHRMLive.java
@@ -46,4 +46,5 @@ public class LoginOrangeHRMLive {
         String driverLocation = System.getProperty("user.dir") + "\\src\\test\\resources\\Driver\\geckodriver.exe";
         System.setProperty("webdriver.gecko.driver", driverLocation);
     }
+
 }
